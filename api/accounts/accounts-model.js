3921ec5a301b90f957861951f8a1e0d224363c79
Chaz Carbis <chazcarbis@gmail.com>
2021-07-12 23:01:00 -0500

    Completed raw sql worksheet and middlewares

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,30 @@
 const getAll = () => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+    return db('accounts')
+        .where('id', id)
+        .update(account)
+        .then((count) => (count > 0 ? get(id) : null));
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+    // DO YOUR MAGIC
 }
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById,
 }
